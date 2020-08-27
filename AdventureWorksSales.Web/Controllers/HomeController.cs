dec788cd5fa82aced1b8cae8e67c6b3ea2bde397
christian kinate <kinate.christian1989@yahoo.com>
2020-08-27 22:37:56 +0100

    Add project files.

---
+++ AdventureWorksSales.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using AdventureWorksSales.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdventureWorksSales.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AdventureWorksSalesEntities1 db = new AdventureWorksSalesEntities1();
+        public ActionResult Index()
+        {
+            //var query = from s in db.SalesOrders join p in db.Products on s.ProductID equals p.ProductID where p.Name.Contains("Front Brakes")
+            //            select new {s.LineTotal };
+            //var TotalFrontBrake = query.Sum();
+
+            var homeVM = new HomeViewModel
+            {
+                TotalOrders = db.SalesOrders.Sum(x => x.OrderQty),
+
+                HighestLineTotal = (int)(db.SalesOrders.Max(m => m.LineTotal)),
+                FrontBrakeSales = (int)(db.SalesOrders.Where(p => p.ProductID == 948).Sum(m => m.LineTotal))
+
+
+            };
+
+
+            return View(homeVM);
+        }
+
+    }
+}
---
+++ AdventureWorksSales.Web/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdventureWorksSales.Web.Models
+{
+    public class HomeViewModel
+    {
+        public int TotalOrders { get; set; }
+        public int HighestLineTotal { get; set; }
+        public int FrontBrakeSales { get; set; }
+    }
+}
